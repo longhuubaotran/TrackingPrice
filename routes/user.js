2d64e24c47af3eb68922a6a7bff1834f0f1e0d98
Long Tran <baolongtran512@gmail.com>
2020-08-08 00:02:47 -0500

    a

--- routes/user.js
+++ routes/user.js
@@ -16,12 +16,6 @@ router.post(
       .isAlphanumeric()
       .isLength({ min: 6 })
       .trim(),
-    body("passwordConfirmation").custom((value, { req }) => {
-      if (value !== req.body.password) {
-        throw new Error("Password confirmation does not match password");
-      }
-      return true;
-    }),
   ],
   userController.createUser
 );
